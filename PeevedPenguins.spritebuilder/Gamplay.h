065f125c606dc595c6d2a019c55d98140a7b68cc
Nikola <nikola@Nikolas-MacBook-Air.local>
2014-06-24 11:57:40 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gamplay.h
@@ -0,0 +1,13 @@
+//
+//  Gamplay.h
+//  PeevedPenguins
+//
+//  Created by Nikola Mijanovik on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gamplay : CCNode
+
+@end
