42b953cccc5f461e3bfe56e5ea17c3daa629521c
Nikola <nikola@Nikolas-MacBook-Air.local>
2014-06-24 13:45:16 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gamplay.m
+++ PeevedPenguins.spritebuilder/Gamplay.m
@@ -47,9 +47,7 @@
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-//    [self runAction:follow];
-    CCActionFollow *followRetry = [CCActionFollow actionWithTarget:penguin worldBoundary:_retryButton.boundingBox];
-    [_retryButton runAction:followRetry];
+    [self runAction:follow];
 }
 
 - (void)retry {
