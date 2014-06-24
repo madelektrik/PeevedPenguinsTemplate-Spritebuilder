982bc83ea149b7ba7080b3a96dbfca7822905c0e
Nikola <nikola@Nikolas-MacBook-Air.local>
2014-06-24 14:07:23 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gamplay.m
+++ PeevedPenguins.spritebuilder/Gamplay.m
@@ -21,6 +21,7 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    _physicsNode.debugDraw = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
 }
