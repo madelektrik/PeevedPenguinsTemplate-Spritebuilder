6225af623c602ae039ee367ac5195071d47066d2
Nikola <nikola@Nikolas-MacBook-Air.local>
2014-06-25 09:34:21 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gamplay.m
+++ PeevedPenguins.spritebuilder/Gamplay.m
@@ -67,19 +67,6 @@
     // start catapult dragging when a touch inside of the catapult arm occurs
     if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
     {
-        // create a penguin from the ccb-file
-        _currentPenguin = [CCBReader load:@"Penguin"];
-        // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
-        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(50,100)];
-        // transform the world position to the node space to which the penguin will be added (_physicsNode)
-        _currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
-        // add it to the physics world
-        [_physicsNode addChild:_currentPenguin];
-        // we don't want the penguin to rotate in the scoop
-        _currentPenguin.physicsBody.allowsRotation = FALSE;
-        
-        // create a joint to keep the penguin fixed to the scoop until the catapult is released
-        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
         
         // move the mouseJointNode to the touch position
         _mouseJointNode.position = touchLocation;
